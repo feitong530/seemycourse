a71a99bccbf2c9414a0f9010ca0aa1c8c187acf5
zy <819628971@qq.com>
2018-11-13 16:37:36 +0800

    change design

--- app/src/main/java/hk/hku/cs/seemycourse/ProductionFragment.java
+++ app/src/main/java/hk/hku/cs/seemycourse/ProductionFragment.java
@@ -17,34 +17,34 @@ public class ProductionFragment extends Fragment {
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
             View view = inflater.inflate(R.layout.frag_production , container, false);
-            initLayout(view);
+//            initLayout(view);
             return view;
         }
 
         private void initLayout(View v) {
-            ImageView a = v.findViewById(R.id.imageView3);
-            a.setImageResource(R.drawable.wjw);
-
-            ImageView b = v.findViewById(R.id.imageView2);
-            b.setImageResource(R.drawable.wjw);
-
-//            ImageView c = v.findViewById(R.id.imageView3);
-//            c.setImageResource(R.drawable.wjw);
+//            ImageView a = v.findViewById(R.id.imageView3);
+//            a.setImageResource(R.drawable.wjw);
 //
-            ImageView d = v.findViewById(R.id.imageView4);
-            d.setImageResource(R.drawable.wjw);
+//            ImageView b = v.findViewById(R.id.imageView2);
+//            b.setImageResource(R.drawable.wjw);
 //
-            ImageView e = v.findViewById(R.id.imageView5);
-            e.setImageResource(R.drawable.wjw);
-//
-            ImageView f = v.findViewById(R.id.imageView6);
-            f.setImageResource(R.drawable.wjw);
-//
-            ImageView g = v.findViewById(R.id.imageView7);
-            g.setImageResource(R.drawable.wjw);
-//
-            ImageView h = v.findViewById(R.id.imageView8);
-            h.setImageResource(R.drawable.wjw);
+////            ImageView c = v.findViewById(R.id.imageView3);
+////            c.setImageResource(R.drawable.wjw);
+////
+//            ImageView d = v.findViewById(R.id.imageView4);
+//            d.setImageResource(R.drawable.wjw);
+////
+//            ImageView e = v.findViewById(R.id.imageView5);
+//            e.setImageResource(R.drawable.wjw);
+////
+//            ImageView f = v.findViewById(R.id.imageView6);
+//            f.setImageResource(R.drawable.wjw);
+////
+//            ImageView g = v.findViewById(R.id.imageView7);
+//            g.setImageResource(R.drawable.wjw);
+////
+//            ImageView h = v.findViewById(R.id.imageView8);
+//            h.setImageResource(R.drawable.wjw);
 
 
 
